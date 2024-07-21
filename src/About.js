83bba8c1da447f799b96d553794a2494eaf13135
DESKTOP-CLM864N\User <orges.lybeshari@gmail.com>
2024-07-21 18:51:01 +0200

    text

--- src/About.js
+++ src/About.js
@@ -5,21 +5,9 @@ function About() {
     return (
       <div id="about">
         <div className="leftSide">
-          <h2>About US</h2>
+          <h2>About The Project</h2>
           <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+            "A demo project made with React where we recieve information about different types of pets throughout the implementation of an API."
           </p>
         </div>
         <div className="rightSide">
